f3d8e4ae121c28f341609f31a82d567c4cc01dd6
Nate Silva <nate@natesilva.com>
2016-11-17 00:55:30 -0800

    index expires to improve cleanup performance

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const co = require('co')
     , mysql = require('co-mysql');
 
-const CREATE_STATEMENT = 'CREATE  TABLE IF NOT EXISTS `_mysql_session_store` (`id` VARCHAR(255) NOT NULL, `expires` BIGINT NULL, `data` TEXT NULL, PRIMARY KEY (`id`));'
     , GET_STATEMENT = 'SELECT * FROM `_mysql_session_store` WHERE id  = ?'
+const CREATE_STATEMENT = 'CREATE  TABLE IF NOT EXISTS `_mysql_session_store` (`id` VARCHAR(255) NOT NULL, `expires` BIGINT NULL, `data` TEXT NULL, PRIMARY KEY (`id`), KEY `_mysql_session_store__expires` (`expires`));'
     , SET_STATEMENT ='INSERT INTO _mysql_session_store(id, expires, data) VALUES(?, ?, ?) ON DUPLICATE KEY UPDATE expires=?, data =?'
     , DELETE_STATEMENT = 'DELETE FROM `_mysql_session_store` WHERE id  = ?'
     , CLEANUP_STATEMENT = 'DELETE FROM `_mysql_session_store` WHERE expires  < ?' ;
